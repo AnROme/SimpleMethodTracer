9cecf404097fd23625d6a6b02070adc3adc9dbd4
AnROme <ana_station@rambler.ru>
2022-04-03 16:31:26 +0300

    Simple tracer implementation

--- SimpleMethodTracer/MethodTracer.cs
+++ SimpleMethodTracer/MethodTracer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,37 @@ using System.Threading.Tasks;
 
 namespace SimpleMethodTracer
 {
-    public class MethodTracer
+    public class MethodTracer : IDisposable
     {
+        private readonly string _className;
+        private readonly string _methodName;
+
+        public MethodTracer()
+        {
+            (_className, _methodName) = GetCallerName();
+
+            Console.WriteLine($"{_className}::{_methodName} -- enter");
+        }
+
+        static private (string, string) GetCallerName()
+        {
+            StackTrace stackTrace = new StackTrace();
+
+            var method = stackTrace.GetFrame(2).GetMethod();
+            string className = method.ReflectedType.Name;
+            string methodName = method.Name;
+            return (className, methodName);
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"{_className}::{_methodName} -- exit");
+        }
+
+        static public void Log(in string message)
+        {
+            var names = GetCallerName();
+            Console.WriteLine($"{names.Item1}::{names.Item2}: {message}");
+        }
     }
 }
